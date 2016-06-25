ad1b7c9d544a42e923009847a3d4d83a382159ef
Brett Celestre <bcelestre@gmail.com>
2016-06-24 19:20:44 -0700

    (feat) Sockets work with chart

--- client/directives/chart.js
+++ client/directives/chart.js
@@ -9,12 +9,6 @@ angular.module('ChartDirective', [])
       replace: true,
       scope: {
         items: '='
-        // options: '=',
-        // one: 0,
-        // two: 0,
-        // three: 0,
-        // four: 0,
-        // five: 0
       },
       controller: function ($scope, $element, $attrs) {
 
@@ -47,20 +41,6 @@ angular.module('ChartDirective', [])
                 text: 'Task Difficulty'
             }
           },
-          // plotOptions: {
-          //   pie: {
-          //     allowPointSelect: true,
-          //     cursor: 'pointer',
-          //     dataLabels: {
-          //       enabled: true,
-          //       color: '#000000',
-          //       connectorColor: '#000000',
-          //       formatter: function () {
-          //         return '<b>' + this.point.name + '</b>: ' + this.percentage + ' %';
-          //       }
-          //     }
-          //   }
-          // },
           series: [{
             // type: 'pie',
             name: 'Browser share',
@@ -68,45 +48,8 @@ angular.module('ChartDirective', [])
           }]
         });
         scope.$watch("items", function (newValue, oldValue) {
-          console.log('newValue: ', newValue);
-          console.log('oldValue: ', oldValue);
           chart.series[0].setData(newValue, true);
-          // chart.redraw();
         }, true);
       }
-      
-      
-      // link: function (scope, element) {
-      //   // Highcharts.chart(element[0], scope.options),
-      //   highchart = new Highcharts.Chart({
-      //   // // Highcharts.chart({
-      //     chart: {
-      //         renderTo: 'chartContainer',
-      //         type: 'column'
-      //     },
-      //     title: {
-      //         text: 'test TITLE'
-      //     },
-      //     xAxis: {
-      //         categories: ['One', 'Two', 'Three', 'Four', 'Five']
-      //     },
-      //     yAxis: {
-      //       max: 5,
-      //       title: {
-      //           text: ''
-      //       }
-      //     },
-      //     legend: {
-      //       enabled: false
-      //     },
-      //     series: [{
-      //         data: scope.votes
-      //         // data: { one: $scope.one, two: $scope.two, three: $scope.three, four: $scope.four, five: $scope.five }
-      //     }]
-      //   });
-      //   scope.$watch('votes', function(newVal){
-      //     highchart.series[0].setData( newVal, true);
-      //   })
-      // },
     };
 });
